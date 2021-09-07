128ca8bc5e8d16cd81902763d33a14ba1f74adb8
Wayne Yen <wayway1974@gmail.com>
2021-09-07 21:16:51 +0800

    優惠價格暫時隱藏

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -48,7 +48,6 @@
         class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
         <a href="javascript: void(0)" class="nav-item" @click="goto('service')">服務優勢</a>
         <a href="javascript: void(0)" class="nav-item" @click="goto('case')">精選案例</a>
-        <a href="javascript: void(0)" class="nav-item" @click="goto('price')">優惠價格</a>
         <a href="javascript: void(0)" class="nav-item" @click="goto('contact')">聯絡我們</a>
       </nav>
     </div>
@@ -164,85 +163,6 @@
           </a>
         </template>
       </div>
-    </div>
-  </section>
-
-  {{-- 優惠價格 --}}
-  <a name="price"></a>
-  <section id="price" class="max-w-screen-lg mx-auto px-4 py-12">
-    <div data-sal="slide-left" class="flex items-center justify-start lg:justify-center text-2xl font-semibold pb-12">
-      <x-heroicon-o-code class="w-10 h-10 mr-2 text-red-600" /> 網壕運優惠價格
-    </div>
-
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8">
-      <div data-sal="zoom-in">
-        <div class="lg:mt-4 rounded-lg bg-blue-100 w-full shadow-lg overflow-hidden h-auto">
-          <div class="px-4 py-8 text-blue-600">
-            <h3 class="mx-auto font-semibold text-center underline leading-relaxed">
-              基礎型
-            </h3>
-            <p class="text-3xl font-bold text-center mt-3">
-              $10,000 元起
-            </p>
-          </div>
-          <div class="bg-blue-500 text-white pl-8 py-6 ">
-            <ul class="list-disc list-inside">
-              <li>RWD 一頁式動態網站設計</li>
-              <li>聯絡表單及管理</li>
-              <li>網站管理後台</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div data-sal="zoom-in">
-        <div class="rounded-lg bg-blue-500 w-full shadow-lg overflow-hidden">
-          <div class="px-4 py-8">
-            <h3 class="mx-auto font-semibold text-center underline leading-relaxed text-white">
-              一般型
-            </h3>
-            <p class="text-3xl font-bold text-center text-white mt-3">
-              $30,000 元起
-            </p>
-          </div>
-
-          <div class="bg-blue-700 text-white pl-8 py-6 ">
-            <ul class="list-disc list-inside">
-              <li>RWD 動態網站設計</li>
-              <li>最新消息頁面及管理</li>
-              <li>文章展示及管理系統</li>
-              <li>服務項目頁面及管理</li>
-              <li>商品展示及管理</li>
-              <li>聯絡表單及管理</li>
-              <li>網站管理後台</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div data-sal="zoom-in">
-        <div class="lg:mt-4 rounded-lg bg-blue-100 w-full shadow-lg overflow-hidden h-auto">
-          <div class="px-4 py-8 text-blue-600">
-            <h3 class="mx-auto font-semibold text-center underline leading-relaxed">
-              客製型
-            </h3>
-            <p class="text-3xl font-bold text-center mt-3 text-yellow-500	">
-              價格優惠中
-            </p>
-          </div>
-          <div class="bg-blue-500 text-white py-6 text-center">
-            <a href="#calc" class="text-2xl inline-block text-white">查看多項優惠</a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <hr class="my-10">
-
-    <a name="calc"></a>
-    <div id="price">
-      <div data-sal="slide-left" class="text-xl text-center">優惠價格試算</div>
-      <price-table></price-table>
     </div>
   </section>
 
